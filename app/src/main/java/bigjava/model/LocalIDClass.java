6591454b773a063191df6219ba968988926e72f6
ntheus <matheusOliveira.7313@gmail.com>
2024-03-18 01:13:04 -0300

    pk de local

---
+++ app/src/main/java/bigjava/model/LocalIDClass.java
@@ -0,0 +1,49 @@
+package bigjava.model;
+
+import java.io.Serializable;
+
+import jakarta.persistence.IdClass;
+
+//@IdClass(value = Local.class)
+public class LocalIDClass implements Serializable {
+
+    private double latitude;
+    private double longitude;
+
+    public LocalIDClass() {
+    }
+
+    public LocalIDClass(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        LocalIDClass other = (LocalIDClass) obj;
+        if (Double.doubleToLongBits(latitude) != Double.doubleToLongBits(other.latitude))
+            return false;
+        if (Double.doubleToLongBits(longitude) != Double.doubleToLongBits(other.longitude))
+            return false;
+        return true;
+    }
+
+}
